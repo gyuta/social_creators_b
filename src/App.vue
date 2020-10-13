e61a921ec15e342dca9b7415bfda292bf109ce23
gyuta <kuroshiba0408@gmail.com>
2020-10-13 15:40:37 +0900

    不要ファイルの削除など

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header></Header>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {Header},
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +21,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: #fff;
+  min-height: 100vh;
+  width: 500px;
+  margin: 0 auto;
 }
 </style>
