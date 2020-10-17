38bda4ad8c4f81f43242b221cef475a044f1434a
gyuta <kuroshiba0408@gmail.com>
2020-10-17 14:12:08 +0900

    ストアの位置情報を表示できるようにした

---
+++ src/components/PinnedMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-if="center">
+      <GmapMap
+        :center="center"
+        :zoom="18"
+        map-type-id="roadmap"
+        style="width: 100%; height: 300px"
+      >
+        <GmapMarker
+          :position="center"
+        />
+      </GmapMap>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PinnedMap',
+  props: ['center']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/StoreList.vue
+++ src/components/StoreList.vue
@@ -1,19 +1,30 @@
 <template>
   <div class='cards'>
+    <Modal ref='map'>
+      <PinnedMap v-if='center' :center='center'></PinnedMap>
+    </Modal>
     <div class="card" v-for="store in stores" :key="store.id">
       {{ store.name }}: {{ store.comment }}
+      <div class="c-btn" v-if="store.center" @click='center = store.center; $refs.map.isActive = true'>
+        位置を確認する
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Modal from './Modal.vue'
+import PinnedMap from './PinnedMap.vue'
+
 export default {
   name: 'StoreList',
   data() {
     return {
-      stores: []
+      stores: [],
+      center: false
     }
   },
+  components: {Modal, PinnedMap},
   async created() {
     const _this = this
     await this.$firebase.firestore().collection('stores')
