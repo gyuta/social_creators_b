b8f6b2ce7cd3d9f2990c9cab45cb4341c0ba3d4d
gyuta <kuroshiba0408@gmail.com>
2020-10-21 13:26:12 +0900

    スタンプカードのハリボテを実装

---
+++ src/components/StampCardModal.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <Modal ref='modal'>
+      <div class="card">
+        <div class="item" v-for="n of 10" :key="n">
+          <div class="cell">
+            {{ n }}
+          </div>
+          <div class='stamp' v-if="n <= count">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-emoji-smile" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path fill-rule="evenodd" d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683z"/>
+              <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class='reward'>
+        {{ reward }}
+      </div>
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from './Modal.vue'
+
+export default {
+  name: 'StampCardModal',
+  components: {Modal},
+  data() {
+    return {
+      count: 3,
+      reward: 'スタンプ１０個獲得でオリジナルTシャツプレゼント！'
+    }
+  },
+  methods: {
+    show() {
+      this.$refs.modal.isActive = true;
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.card {
+  display: flex;
+  flex-wrap: wrap;
+
+  .item {
+    flex-basis: 20%;
+    padding: 5px;
+    height: 60px;
+    position: relative;
+
+    .cell {
+      border: black solid 1px;
+      height: 50px;
+      margin: 5px;
+      border-radius: 10px;
+      line-height: 40px;
+    }
+    
+    &:last-of-type {
+      .cell {
+        border: var(--yellow) solid 2px;
+        font-weight: bold;
+      }
+    }
+
+    .stamp {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: white;
+      margin-top: 8px;
+
+      svg {
+        width: 30px;
+        height: 30px;
+      }
+    }
+  }
+}
+
+.reward {
+  margin-top: 10px;
+  font-size: 12px;
+}
+</style>
--- src/views/StoreDetail.vue
+++ src/views/StoreDetail.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <StampCardModal ref='stamp'></StampCardModal>
     <div class="main-imgs">
       <carousel :per-page="1" :centerMode='true' :loop="true" :paginationEnabled='false' :autoplay="true">
         <slide v-for="img in store.images" :key='img'>
@@ -14,6 +15,11 @@
         <div class="item">営業日：毎週日曜日定休日</div>
         <div class="item">価格帯：50円〜10000円</div>
         <div class="item">住所：京都市左京区京都らしさ町挨拶１丁目</div>
+      </div>
+      <div class="stamp-btn" @click='$refs.stamp.show()'>
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-gift" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506V2.5zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07zM9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0V3zM1 4v2h6V4H1zm8 0v2h6V4H9zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5V7zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5H7z"/>
+        </svg>
       </div>
     </div>
     <div class="store-info">
@@ -61,6 +67,8 @@
 </template>
 
 <script>
+import StampCardModal from '../components/StampCardModal'
+
 export default {
   name: 'StoreDetail',
   data() {
@@ -73,6 +81,7 @@ export default {
       commentBody: ''
     }
   },
+  components: {StampCardModal},
   props: ['loginUser'],
   async created() {
     const _this = this
@@ -166,6 +175,7 @@ export default {
 
 .store-header {
   padding: 10px;
+  position: relative;
   
   .name {
     margin-left: 10px;
@@ -183,6 +193,21 @@ export default {
   .detail {
     font-size: 8px;
     margin-left: 20px;
+  }
+
+  .stamp-btn {
+    position: absolute;
+    bottom: 10px;
+    right: 20px;
+    border: black solid 1px;
+    border-radius: 20px;
+    padding: 0 4px;
+    cursor: pointer;
+
+    svg {
+      position: relative;
+      top: 2px;
+    }
   }
 }
 
