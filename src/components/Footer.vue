55a0c7420bfe347418ea1cd3d538eff050a6eb46
gyuta <kuroshiba0408@gmail.com>
2020-10-21 10:45:29 +0900

    footer にアイコンを追加

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="footer-wrap">
     <div id="footer">
+      <div class="item">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
+          <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+        </svg>
+      </div>
+      <div class="item">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+          <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+        </svg>
+      </div>
+      <div class="item">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+        </svg>
+      </div>
+      <div class="item">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -11,7 +33,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang='scss'>
 #footer-wrap {
   height: var(--footer-height);
 }
@@ -24,5 +46,19 @@ export default {
   width: 100%;
   max-width: var(--max-width);
   bottom: 0;
+  display: flex;
+  align-items: center;
+
+  .item {
+    flex-basis: 25%;
+    text-align: center;
+    cursor: pointer;
+
+    svg {
+      width: 40px;
+      height: 40px;
+      fill: white;
+    }
+  }
 }
 </style>
