d8c322675030ab4ff9060cec51ed93b82642ae81
gyuta <kuroshiba0408@gmail.com>
2020-10-23 21:21:42 +0900

    ログイン周りのUI変更

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
-    <h1>ログイン</h1>
-    <div class="field">
-      <label>メールアドレス</label>
-      <input type="text" placeholder=" " v-model="email" />
+    <Title title='ログイン' :showBackBtn="false"></Title>
+    <div class="main">
+      <div class="field">
+        <label>メールアドレス</label>
+        <input type="text" placeholder=" " v-model="email" />
+      </div>
+      <div class="field">
+        <label>パスワード</label>
+        <input type="password" placeholder=" " v-model="password" />
+      </div>
+      <div class='btn' @click='signin'>ログインする</div>
+      <div class="btn new" @click="$router.push('signup')">新規登録する</div>
     </div>
-    <div class="field">
-      <label>パスワード</label>
-      <input type="password" placeholder=" " v-model="password" />
-    </div>
-    <div class='c-btn' @click='signin'>ログインする</div>
   </div>
 </template>
 
 <script>
+import Title from '../components/Title.vue'
+
 export default {
   name: 'Signin',
+  components: {Title},
   data() {
     return {
       email: '',
@@ -37,6 +43,49 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang='scss'>
+
+.main {
+  padding: 20px;
+}
+
+label {
+  display: block;
+  margin-top: 20px;
+}
+.field {
+  input {
+    padding-left: 20px;
+    border-radius: 40px;
+    outline: none;
+    border: black solid 1px;
+    margin-left: 20px;
+    width: 280px;
+
+  }
+}
+
+.field2 {
+  margin-top: 20px;
+}
+
+.btn {
+  cursor: pointer;
+  width: 200px;
+  background-color: var(--yellow);
+  text-align: center;
+  margin: 0 auto;
+  padding: 4px 8px;
+  color: white;
+  border-radius: 10px;
+  margin-top: 40px;
+}
+
+.new {
+  background-color: white;
+  color: var(--yellow);
+  border: 1px solid var(--yellow);
+  margin-top: 20px;
+}
 
 </style>
